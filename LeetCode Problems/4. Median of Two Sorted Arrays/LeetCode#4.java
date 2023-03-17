f1a7e660a6a9cd69cce042f915a9b1a9e2fd0724
Claudia <57412832+ycheng03@users.noreply.github.com>
2023-03-17 14:02:46 -0600

    Added solutions to 2 LeetCode Problems

---
+++ LeetCode Problems/4. Median of Two Sorted Arrays/LeetCode#4.java
@@ -0,0 +1,51 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+
+        // To ensure that nums1 is the smaller array.
+        if (m > n) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
+        int low = 0, high = m;
+        while (low <= high) {
+            int i = (low + high) / 2;
+            int j = (m + n + 1) / 2 - i;
+
+            if (i < m && nums2[j - 1] > nums1[i]) {
+                low = i + 1;
+            }
+            else if (i > 0 && nums1[i - 1] > nums2[j]) {
+                high = i - 1;
+            }
+            else {
+                int maxLeft;
+                if (i == 0) {
+                    maxLeft = nums2[j - 1];
+                } else if (j == 0) {
+                    maxLeft = nums1[i - 1];
+                } else {
+                    maxLeft = Math.max(nums1[i - 1], nums2[j - 1]);
+                }
+
+                if ((m + n) % 2 == 1) {
+                    return maxLeft;
+                }
+
+                int minRight;
+                if (i == m) {
+                    minRight = nums2[j];
+                } else if (j == n) {
+                    minRight = nums1[i];
+                } else {
+                    minRight = Math.min(nums1[i], nums2[j]);
+                }
+
+                return (maxLeft + minRight) / 2.0;
+            }
+        }
+        
+        return 0.0;
+    }
+}
